b3baf1341bbb0046948aa3c3421b826baac62dfe
antonio <antoniogomesdev@gmail.com>
2022-08-23 16:25:07 -0300

    form validation added

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -5,6 +5,25 @@ import router from "../router/index.js";
 import axios from "axios";
 
 const emailInfos = ref({});
+const pattern =
+  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+function isButtonDisabled() {
+  if (
+    !emailInfos.value.from_name ||
+    !emailInfos.value.reply_to ||
+    !emailInfos.value.message
+  )
+    return true;
+  console.log(pattern.test(emailInfos.value.reply_to));
+  return pattern.test(emailInfos.value.reply_to);
+}
+const rules = {
+  required: (value) => !!value || "Campo obrigatório.",
+  counter: (value) => value.length <= 20 || "Max 20 characters",
+  email: (value) => {
+    return !pattern.test(value) || "E-mail inválido.";
+  },
+};
 
 const onSendEmailClick = () => {
   axios
@@ -12,7 +31,7 @@ const onSendEmailClick = () => {
       service_id: "service_qlhnalf",
       template_id: "template_duey9ac",
       user_id: "mpUBQi_YKZSGRfpYd",
-      template_params: emailInfos.value,
+      template_params: emailInfos,
     })
     .then((res) => {
       console.log(res);
@@ -39,19 +58,22 @@ const onSendEmailClick = () => {
                 <p class="subtitle-text">Nome</p>
                 <v-text-field
                   v-model="emailInfos.from_name"
+                  :rules="[rules.required]"
                   class="form-field"
-                  density="compact"
                   variant="outlined"
+                  density="compact"
                 ></v-text-field>
                 <p class="subtitle-text">Email</p>
                 <v-text-field
                   v-model="emailInfos.reply_to"
+                  :rules="[rules.required, rules.email]"
                   class="form-field"
                   density="compact"
                   variant="outlined"
                 ></v-text-field>
                 <p class="subtitle-text">Mensagem</p>
                 <v-textarea
+                  :rules="[rules.required]"
                   v-model="emailInfos.message"
                   density="compact"
                   variant="outlined"
@@ -63,8 +85,10 @@ const onSendEmailClick = () => {
           <v-row justify="end">
             <v-spacer />
             <v-spacer />
-            <div @click="onSendEmailClick">
-              <OnHoverButton content="Enviar" />
+            <div>
+              <v-btn @click="onSendEmailClick" :disabled="isButtonDisabled()">
+                Enviar
+              </v-btn>
             </div>
             <v-spacer />
           </v-row>
